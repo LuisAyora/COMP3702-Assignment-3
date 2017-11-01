7f2519374b5f8a906b227bf741d573b7a2b20897
caet <silvia.erazotorres@uq.net.au>
2017-11-01 12:22:46 +1000

    three for loops  On branch master

--- Assignment-3/src/solver/MySolver.java
+++ Assignment-3/src/solver/MySolver.java
@@ -144,3 +144,22 @@ public class MySolver implements FundingAllocationAgent {
 		return prob;
 	}
 }
+
+int i=0;
+int j=0;
+int k=0;
+int numMax = 5;
+List<ArrayList<Integer>>Number = new ArrayList<ArrayList<Integer>>();	
+//List<Integer>Number=new ArrayList<Integer>();
+
+for (i=0;i<=numMax;i++) {
+	for (j=0;j<=numMax-i;j++) {
+		for (k=0; k <=numMax-i-j; k++) {
+			ArrayList<Integer>sublist=new ArrayList<Integer>();
+			//ArrayList<Integer> sublist = new ArrayList<ArrayList<Integer>>();
+			sublist.add(i);
+			sublist.add(j);
+			sublist.add(k);
+			Number.add(sublist);
+			System.out.println(sublist);
+	}
